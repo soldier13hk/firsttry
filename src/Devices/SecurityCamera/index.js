278441db3702080c087a2ab88d11cb92f8a75869
Ho Shu Kwan <hoshukwan1@gmail.com>
2018-03-17 14:49:46 +0800

    Get image from server

--- src/Devices/SecurityCamera/index.js
+++ src/Devices/SecurityCamera/index.js
@@ -3,16 +3,81 @@ import {
     StyleSheet,
     View,
     Text,
+    ActivityIndicator,
+    Image
 } from 'react-native';
 import * as Colors from '../Themes/colors';
+import {IP} from '../../constants/serverIP';
 
 class HomeScreen extends Component {
+    constructor(props){
+        super(props);
+        this.state ={ isLoading: true}
+    }
+    componentDidMount(){
+        //https://facebook.github.io/react-native/movies.json
+        return fetch('http://'+IP+'/imagelists')
+            .then((response) => response.json())
+            .then((responseJson) => {
+
+                this.setState({
+                    isLoading: false,
+                    dataSource: responseJson,
+                }, function(){
+
+                });
+
+            })
+            .catch((error) =>{
+                console.error(error);
+            });
+
+    }
     render() {
-        return (
-            <View style={styles.container}>
-                <Text style={{ textAlign: 'center', color: Colors.primary, marginRight: 10 }}>Security Camera Screen</Text>
-            </View>
-        );
+        // if(this.state.dataSource.images){
+        //     console.log('SecurityCamera/index: '+this.state.dataSource.images);
+        // }
+
+        if(this.state.isLoading){
+            return(
+                <View style={{flex: 1, padding: 20}}>
+                    <ActivityIndicator/>
+                    <View style={styles.container}>
+                        <Text style={{ textAlign: 'center', color: Colors.primary, marginRight: 10 }}>Timer Switch Screen</Text>
+                    </View>
+                </View>
+            );
+        }
+        else{
+            console.log('SecurityCamera/index: '+ this.state.dataSource.images);
+            //let obj = JSON.parse(this.state.dataSource.images);
+            // let element = 'favicon.png';
+            // for (var key in this.state.dataSource.images) return (
+            //     <View>
+            //     <Image
+            //         style={{width: 100, height: 100}}
+            //         //source={{url: 'http://10.89.182.20:3000/'+element}}
+            //         source={{uri: 'https://facebook.github.io/react-native/docs/assets/favicon.png'}}
+            //     />
+            //     </View>
+            // );
+            return(
+                <View>
+                    {this.state.dataSource.images.map(function(val,key){
+                        console.log('securityCamera/index: '+key+' , '+val+'type: '+ typeof val);
+                        return <View>
+                            <Image
+                                style={{width: 100, height: 100}}
+                                // source={{uri: '10.89.182.20:3000/img_fjords.jpg'}}
+                                // source={{uri: 'https://facebook.github.io/react-native/docs/assets/favicon.png'}}
+                                source = {{uri: 'http://'+ IP+'/'+val}}
+
+                            />
+                        </View>
+                    })}
+                </View>
+            );
+        }
     }
 }
 
